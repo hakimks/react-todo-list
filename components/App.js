2b0d211b8e0d660a95720107ea336441c823aa40
hakimks <hakimks@gmail.com>
2019-03-04 12:18:44 +0300

    Feat: add webpack

--- components/App.js
+++ components/App.js
@@ -1,1 +1,11 @@
+import React, {Component} from 'react'
 
+
+class App extends Component {
+    render() {
+        return <div>This is definitely a react component Now!</div>
+    }
+
+}
+
+export default App;
--- webpack.config.js
+++ webpack.config.js
@@ -1,1 +1,23 @@
+const path = require('path');
 
+module.exports = {
+    mode: "development",
+    entry: ['./client/client.js'],
+    output: {
+        path: path.resolve(__dirname,"dist"),
+        filename: 'bundle.js',
+        publicPath: '/'
+    }, 
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                loader: 'babel-loader',
+                exclude: /node_modules/,
+                query: {
+                    presets: ['react', 'es2015']
+                }
+            }
+        ]
+    }
+}
